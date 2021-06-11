614e92bf80ef4786f7e751b95acacd9401bdfd86
Devi Vikas Acharyulu Pulluri <devivikas.pulluri1@t-mobile.com>
2021-06-11 16:54:55 +0530

    Typescript Updated

--- lists/Node.js
+++
@@ -1,4 +0,0 @@
-export default function Node(x) {
-  this.data = x;
-  this.next = null;
-}
--- tests/helper/isSorted.js
+++
@@ -1,9 +0,0 @@
-module.exports = (arr) => {
-  for (let i = 1;i < arr.length;i++) {
-    if (arr[i] < arr[i-1]) {
-      return false;
-      break;
-    }
-  }
-  return true;
-}
