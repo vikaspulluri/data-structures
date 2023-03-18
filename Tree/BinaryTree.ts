4d006b59affddb831c0bd474e17fb09bc877213d
vikaspulluri <vikasiiitn@gmail.com>
2023-03-18 11:45:48 +0530

    Tree updates

--- Tree/BinaryTree.ts
+++ Tree/BinaryTree.ts
@@ -288,9 +288,9 @@ export class BinaryTree {
     return true;
   };
 
-  rightSideView(root: Node | null): number[] {
+  rightSideView(): number[] {
     const results = [];
-    const q = [root];
+    const q = [this.root];
     while(q.length > 0) {
         const size = q.length;
         for (let i=0;i<size;i++) {
